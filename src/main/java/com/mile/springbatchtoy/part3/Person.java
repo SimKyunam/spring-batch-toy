16dae42eab1739e04adc1cef4e9eefb2fca24602
kyunam-sim <ppp9026@gmail.com>
2022-10-18 23:34:35 +0900

    itemReader 공부

---
+++ src/main/java/com/mile/springbatchtoy/part3/Person.java
@@ -0,0 +1,15 @@
+package com.mile.springbatchtoy.part3;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Person {
+    private int id;
+    private String name;
+    private String age;
+    private String address;
+}
