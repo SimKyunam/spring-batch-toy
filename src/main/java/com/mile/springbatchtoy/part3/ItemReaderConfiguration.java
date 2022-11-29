111ed934e3a9f814290927c6cbec7f84af580531
kyunam-sim <ppp9026@gmail.com>
2022-11-29 23:17:09 +0900

    jdbcCursorItemReader, jdbcPagingItemReader 학습코드 작성

--- src/main/java/com/mile/springbatchtoy/part3/ItemReaderConfiguration.java
+++ src/main/java/com/mile/springbatchtoy/part3/ItemReaderConfiguration.java
@@ -8,6 +8,12 @@ import org.springframework.batch.core.configuration.annotation.JobBuilderFactory
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.launch.support.RunIdIncrementer;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.item.database.JdbcCursorItemReader;
+import org.springframework.batch.item.database.JdbcPagingItemReader;
+import org.springframework.batch.item.database.PagingQueryProvider;
+import org.springframework.batch.item.database.builder.JdbcCursorItemReaderBuilder;
+import org.springframework.batch.item.database.builder.JdbcPagingItemReaderBuilder;
+import org.springframework.batch.item.database.support.SqlPagingQueryProviderFactoryBean;
 import org.springframework.batch.item.file.FlatFileItemReader;
 import org.springframework.batch.item.file.builder.FlatFileItemReaderBuilder;
 import org.springframework.batch.item.file.mapping.DefaultLineMapper;
@@ -15,9 +21,13 @@ import org.springframework.batch.item.file.transform.DelimitedLineTokenizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
 
+import javax.sql.DataSource;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Configuration
@@ -27,6 +37,7 @@ public class ItemReaderConfiguration {
 
     private final JobBuilderFactory jobBuilderFactory;
     private final StepBuilderFactory stepBuilderFactory;
+    private final DataSource dataSource;
 
     @Bean
     public Job itemReaderJob() throws Exception {
@@ -35,6 +46,8 @@ public class ItemReaderConfiguration {
                 .start(this.customItemReaderStep())
                 .next(this.csvFileStep())
                 .next(this.csvFileBulkStep())
+                .next(this.jdbcStep())
+                .next(this.jdbcPagingStep())
                 .build();
     }
 
@@ -54,6 +67,80 @@ public class ItemReaderConfiguration {
                 .reader(this.csvFileItemReader())
                 .writer(itemWriter())
                 .build();
+    }
+
+    @Bean
+    public Step jdbcStep() throws Exception {
+        return stepBuilderFactory.get("jdbcStep")
+                .<Person, Person>chunk(10)
+                .reader(jdbcCursorItemReader())
+                .writer(itemWriter())
+                .build();
+    }
+
+    @Bean
+    public Step jdbcPagingStep() throws Exception {
+        return stepBuilderFactory.get("jdbcPagingStep")
+                .<Person, Person>chunk(10)
+                .reader(jdbcPagingItemReader())
+                .writer(itemWriter())
+                .build();
+    }
+
+    private JdbcCursorItemReader<Person> jdbcCursorItemReader() throws Exception {
+        JdbcCursorItemReader<Person> itemReader = new JdbcCursorItemReaderBuilder<Person>()
+                .name("jdbcCursorItemReader")
+                .dataSource(dataSource)
+                .sql("select id, name, age, address from person")
+                .rowMapper((rs, rowNum) -> new Person(
+                        rs.getInt(1)
+                        , rs.getString(2)
+                        , rs.getString(3)
+                        , rs.getString(4)))
+                .build();
+
+        // afterPropertiesSet() 각각의 Writer 실행되기 위해 필요한 필수값들이 제대로 세팅되어있는지를 체크
+        itemReader.afterPropertiesSet();
+        return itemReader;
+    }
+
+    @Bean
+    public JdbcPagingItemReader<Person> jdbcPagingItemReader() throws Exception {
+        //jdbcPagingItemReader, queryProvider는 Bean으로 등록되야 동작 함..
+
+        Map<String, Object> parameterValues = new HashMap<>();
+        parameterValues.put("name", "이경원");
+
+        JdbcPagingItemReader<Person> jdbcPagingItemReader = new JdbcPagingItemReaderBuilder<Person>()
+                .name("jdbcPagingItemReader")
+                .pageSize(10)
+                .fetchSize(10)
+                .dataSource(dataSource)
+                .rowMapper((rs, rowNum) -> new Person(
+                        rs.getInt(1)
+                        , rs.getString(2)
+                        , rs.getString(3)
+                        , rs.getString(4)))
+                .queryProvider(queryProvider())
+                .parameterValues(parameterValues)
+                .build();
+
+        // afterPropertiesSet() 각각의 Writer 실행되기 위해 필요한 필수값들이 제대로 세팅되어있는지를 체크
+        jdbcPagingItemReader.afterPropertiesSet();
+        return jdbcPagingItemReader;
+    }
+
+    @Bean
+    public PagingQueryProvider queryProvider() throws Exception {
+        SqlPagingQueryProviderFactoryBean provider = new SqlPagingQueryProviderFactoryBean();
+
+        provider.setDataSource(dataSource);
+        provider.setSelectClause("id, name, age, address");
+        provider.setFromClause("from person");
+        provider.setWhereClause("where name=:name");
+        provider.setSortKey("id");
+
+        return provider.getObject();
     }
 
     @Bean
